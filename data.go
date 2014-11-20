dae09a8c9dc0832c254e7aaf188b52c2e3b4cc9d
Will Norris <will@willnorris.com>
2014-11-19 22:02:34 -0800

    rename proxy.go and remove old data package doc

--- data.go
+++ data.go
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package data provides common shared data structures for imageproxy.
 package imageproxy
 
 import (
