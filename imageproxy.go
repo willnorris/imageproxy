a1fa81145cbca571c9a9b1070c715650e90b21bc
Wilrik <wmook@050media.nl>
2018-07-06 16:36:06 +0200

    fixed tcp_mem resource leak when sending 304

--- imageproxy.go
+++ imageproxy.go
@@ -315,12 +315,13 @@ func (t *TransformingTransport) RoundTrip(req *http.Request) (*http.Response, er
 		return nil, err
 	}
 
+	defer resp.Body.Close()
+
 	if should304(req, resp) {
 		// bare 304 response, full response will be used from cache
 		return &http.Response{StatusCode: http.StatusNotModified}, nil
 	}
 
-	defer resp.Body.Close()
 	b, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return nil, err
