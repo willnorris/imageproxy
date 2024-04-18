7976a78c207ce7438d698f63e5e765273132673c
Ken Kelly <ken@kenhkelly.us>
2024-04-18 14:12:22 -0400

    DEVOPS-1073: Fix how we handle 304s and >= 400s

--- perf-test/randomized_test.js
+++ perf-test/randomized_test.js
@@ -1,37 +1,47 @@
-import http from 'k6/http';
-import {check, sleep} from 'k6';
-import {getRandomUrl} from "./helpers";
-
+import http from "k6/http";
+import { check, sleep } from "k6";
+import { getRandomUrl } from "./helpers";
 
 export const options = {
-    scenarios: {
-        'imageproxy_images_randomized': {
-            executor: 'ramping-vus',
-            startVUs: 0,
-            stages: [{duration: '10s', target: 200}, {duration: '60s', target: 600}, {duration: '10s', target: 300},],
-            gracefulRampDown: '5s',
-        }
-    }, thresholds: {
-        http_req_duration: ['p(95)<200'], // 95% of requests should be below 200ms
-        http_req_failed: ['rate<0.01'], // http errors should be less than 1%
-
+  scenarios: {
+    imageproxy_images_randomized: {
+      executor: "ramping-vus",
+      startVUs: 0,
+      stages: [
+        { duration: "10s", target: 20 },
+        { duration: "60s", target: 60 },
+        { duration: "10s", target: 30 },
+      ],
+      gracefulRampDown: "5s",
     },
+  },
+  thresholds: {
+    http_req_duration: ["p(95)<200"], // 95% of requests should be below 200ms
+    http_req_failed: ["rate<0.01"], // http errors should be less than 1%
+  },
 };
 
 const params = {
-    tags: {name: 'imageproxy'},
-}
+  tags: { name: "imageproxy" },
+};
 
 export default function () {
-    const url =getRandomUrl(true);
-    const res = http.get(url, { ...params });
+  const url = getRandomUrl(true);
+  const res = http.get(url, {
+    ...params,
+    headers: {
+      //   "if-modified-since": "Tue, 20 Feb 2024 04:48:19 GMT",
+    },
+  });
 
-    check(res, {'is status 200': r => {
-            // if (r.status === 200) return true;
-            // console.log(r.status, r.url);
-            // return false;
-            return r.status === 200;
-        }});
+  check(res, {
+    "is status 200": (r) => {
+      console.log(r.status, r.url);
+      if (r.status === 200) return true;
+      return false;
+      //   return r.status === 200;
+    },
+  });
 
-    sleep(1);
+  sleep(1);
 }
