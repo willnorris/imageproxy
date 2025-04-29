82ce506905db83a8be6f8af061f126c103409d18
Will Norris <will@willnorris.com>
2025-04-28 23:28:37 -0700

    third_party/httpcache: add copy of parts of httpcache

---
+++ third_party/httpcache/httpcache.go
@@ -0,0 +1,26 @@
+package httpcache
+
+import (
+	"net/http"
+	"strings"
+)
+
+type cacheControl map[string]string
+
+func parseCacheControl(headers http.Header) cacheControl {
+	cc := cacheControl{}
+	ccHeader := headers.Get("Cache-Control")
+	for _, part := range strings.Split(ccHeader, ",") {
+		part = strings.Trim(part, " ")
+		if part == "" {
+			continue
+		}
+		if strings.ContainsRune(part, '=') {
+			keyval := strings.Split(part, "=")
+			cc[strings.Trim(keyval[0], " ")] = strings.Trim(keyval[1], ",")
+		} else {
+			cc[part] = ""
+		}
+	}
+	return cc
+}
